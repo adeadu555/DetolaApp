ad651c9121b265299a23710dbd98ee5211b58a2b
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 08:47:33 -0500

    updated App.js

--- detolaapp/src/App.js
+++ detolaapp/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
@@ -9,7 +9,17 @@ import Contact from "./components/pages/Contact";
 
 function App(){
 
-    return ();
+    return (
+        <Router>
+      <div>
+        <NavTabs />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/blog" component={Blog} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+    );
 }
 
 export default App;
