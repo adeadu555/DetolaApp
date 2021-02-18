a09af221d75296649616aaf5f1e837a3f51abe9a
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 08:52:34 -0500

    updated Contact.js file

--- detolaapp/src/components/pages/Contact.js
+++ detolaapp/src/components/pages/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 
 
-function Contact(){
+function Contact(props){
     return (
         <div>
             <h1>Contact Page</h1>
@@ -11,6 +11,14 @@ function Contact(){
 
                 Detola App
             </p>
+    <Link to={`${props.match.url}/learn`} role="button" className="btn btn-link">
+        Learn More
+      </Link>{" "}
+      <Link to="/contact" role="button" className="btn btn-link">
+        Learn Less
+      </Link>
+      <Route exact path={`${props.match.url}/learn`} component={Learn} />
+
         </div>
     );
 }
