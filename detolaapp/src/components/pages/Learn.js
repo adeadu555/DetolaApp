62acfb5b9c8bb1ad1179642410814d851605296b
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 07:10:30 -0500

    created new page files

---
+++ detolaapp/src/components/pages/Learn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Learn() {
+    return (
+        <div>
+            <h1>Hello</h1>
+            <p>
+                Detola App
+            </p>
+        </div>
+    );
+}
+
+export default Learn;
