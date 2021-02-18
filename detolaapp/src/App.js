0d07ecd6bf202942718b4dc67c7beab1ae025266
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 07:18:42 -0500

    updated App.js file

---
+++ detolaapp/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Contact from "./components/pages/Contact";
+
+
+function App(){
+
+    return ();
+}
+
+export default App;
