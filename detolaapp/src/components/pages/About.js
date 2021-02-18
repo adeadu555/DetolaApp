a9601e3d04ded635263904270b943ac9e2fe3d7f
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 06:50:11 -0500

    updated About.js

---
+++ detolaapp/src/components/pages/About.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function About(){
+    return (
+        <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    Navbar
+                </a>
+                <button
+                className="navbar-toggler"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon" />
+                    </button>
+            </nav>
+        </div>
+
+    );
+}
+
+export default About;
