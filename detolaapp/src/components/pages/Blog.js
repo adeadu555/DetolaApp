bdb0659123bcf19d35bf611997e7d6936fbff8c1
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 14:24:59 -0500

    updated files

--- detolaapp/src/components/pages/Blog.js
+++ detolaapp/src/components/pages/Blog.js
@@ -2,13 +2,29 @@ import React from "react";
 
 function Blog(){
     return (
+        <div className="container">
+        <div className="jumbotron">
         <div>
-        <h1>Blog Page</h1>
+        <h1>Blog</h1>
         <p>
             Detola App
         </p>
+        </div>
 
         </div>
+        <div className="card">
+            <div className="card-header">
+               <h3 className="card-title">Blog</h3> 
+            </div>
+            <div className="card-body">
+                <p className="card-text">Detola App</p>
+            </div>
+
+        </div>
+    
+    </div>
+
+    
         
     );
 }
--- detolaapp/src/components/pages/Home.js
+++ detolaapp/src/components/pages/Home.js
@@ -12,6 +12,15 @@ function Home() {
             </div>
 
             </div>
+            <div className="card">
+                <div className="card-header">
+                   <h3 className="card-title">Home</h3> 
+                </div>
+                <div className="card-body">
+                    <p className="card-text">Detola App</p>
+                </div>
+
+            </div>
         
         </div>
 
