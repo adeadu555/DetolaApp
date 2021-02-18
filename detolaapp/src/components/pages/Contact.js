18206becebec2c5968be045d609ac34eaeac1772
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 07:04:35 -0500

    created Contact.js file

---
+++ detolaapp/src/components/pages/Contact.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+function Contact(){
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>
+
+
+                Detola App
+            </p>
+        </div>
+    );
+}
+
+
+export default Contact;
