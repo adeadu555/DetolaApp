83b76ced60c061ae0b8cc82c5e4780d693c3382f
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-18 07:00:28 -0500

    created Blog.js file

---
+++ detolaapp/src/components/pages/Blog.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Blog(){
+    return (
+        <div>
+        <h1>Blog Page</h1>
+        <p>
+            Detola App
+        </p>
+
+        </div>
+        
+    );
+}
+
+export default Blog;
