6dba075d0c6033af06e2efa6ac2af67c769288e0
Adegboyega Adu <gboyega@Adegboyegas-MacBook-Pro.local>
2021-02-19 15:59:48 -0500

    updated files

--- detolaapp/src/utils/API.js
+++ detolaapp/src/utils/API.js
@@ -1,1 +1,11 @@
 import axios from "axios";
+
+const BASEURL = "https://www.omdbapi.com/?t=";
+const APIKEY = "";
+
+
+export default {
+    search: function(query) {
+      return axios.get(BASEURL + query + APIKEY);
+    }
+  };
